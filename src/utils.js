d2efd35d33e1dec5ca1771803fd6e82cb01ef184
Dylan <dylan@Dylans-MacBook.local>
2018-01-11 16:28:13 -0500

    fixed contact click

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export async function sendEmail(msg) {
   try {
+    await axios.get('https://send-email-portfolio.herokuapp.com/');
     const res = await axios.post('https://send-email-portfolio.herokuapp.com/sendEmail', { msg });
     return res;
   } catch(err) {
