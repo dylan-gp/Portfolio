2614d06523df09b421d62b5d6701d2a4a820826c
Dylan <dylan@Dylans-MacBook.local>
2018-01-10 19:04:02 -0500

    contact form works now

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function sendEmail(msg) {
   try {
-    const res = await axios.post('http://send-email-portfolio.herokuapp.com/sendEmail', { msg });
+    const res = await axios.post('https://send-email-portfolio.herokuapp.com/sendEmail', { msg });
     return res;
   } catch(err) {
     return err;
